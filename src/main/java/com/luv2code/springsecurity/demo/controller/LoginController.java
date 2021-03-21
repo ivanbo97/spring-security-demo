1bd3b0096cb64006a169e5223b8e56dc60a9582e
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-21 11:01:23 +0200

    New login page using bootstrap.

--- src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -3,12 +3,14 @@ package com.luv2code.springsecurity.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.luv2code.springsecurity.demo.util.ViewNames;
+
 @Controller
 public class LoginController {
 
 	@GetMapping("/showMyLoginPage")
 	public String showMyLoginPage() {
 		
-		return "plain-login";
+		return ViewNames.BOOTSTRAP_LOGIN;
 	}
 }
--- src/main/java/com/luv2code/springsecurity/demo/util/ViewNames.java
+++ src/main/java/com/luv2code/springsecurity/demo/util/ViewNames.java
@@ -3,6 +3,7 @@ package com.luv2code.springsecurity.demo.util;
 public final class ViewNames {
 
 	public static final String HOME = "home";
+	public static final String BOOTSTRAP_LOGIN = "bootstrap-login";
 	
 	private ViewNames() {
 		
